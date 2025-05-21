daf7527dac8f3343a45c56dadd0287423389f08d
Guardianff <103484627+Guardianff@users.noreply.github.com>
2025-05-21 23:40:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -16,7 +16,6 @@ from datetime import datetime, date
 from typing import List
 from database.users_chats_db import db
 from bs4 import BeautifulSoup
-import requests
 import aiohttp
 from pyrogram.errors import ChatAdminRequired
 
