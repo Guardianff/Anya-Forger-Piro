fdef1555319186cc3312992ce3b952e9dd5757a6
Guardianff <103484627+Guardianff@users.noreply.github.com>
2025-05-23 13:17:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1259,7 +1259,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     
 async def auto_filter(client, msg, spoll=False):
-    reqstr1 = msg.from_user.id if msg.from_user else 0
+    reqstr1 = msg.from_user.id if msg.from_user else None
     reqstr = await client.get_users(reqstr1)
     if not spoll:
         message = msg
